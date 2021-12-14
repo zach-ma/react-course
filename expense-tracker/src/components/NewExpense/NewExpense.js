37b9d5b93fa6c84a1f4564de2ad38606114b5bd3
zach-ma <mjx.ma@icloud.com>
2021-12-14 15:57:00 -0500

    react state & working with events

---
+++ expense-tracker/src/components/NewExpense/NewExpense.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
+
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    console.log(expenseData);
+
+    props.onAddExpense(expenseData); // NOTE
+  };
+
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    </div>
+  );
+};
+
+export default NewExpense;
